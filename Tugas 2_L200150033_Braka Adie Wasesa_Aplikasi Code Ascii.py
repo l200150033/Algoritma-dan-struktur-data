1d1cf678b97953da3f5bf974a00be2414bb74608
Braka Adie Wasesa <adie.raka96@gmail.com>
2017-03-27 00:47:49 +0700

    Add files via upload

---
+++ Tugas 2_L200150033_Braka Adie Wasesa_Aplikasi Code Ascii.py
@@ -0,0 +1,6 @@
+Karakter = raw_input("Masukkan sebuah karakter berupa huruf, angka, maupun simbol : ")
+        ## meminta input yang hasilnya disimpan di variabel Karakter
+Nomor = ord(Karakter) ##melakukan konversi dari input yang disimpan di variabel Karakter ke kode ascii
+print("Kode dari karakter "+ Karakter + " adalah "+ str(Nomor)+"\n") ##menampilkan hasil
+
+ 
